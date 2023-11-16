a62c1b15f1b791310f345e36d1ed74f84e1a1da0
Assaf Koren <assafnoahkoren@gmail.com>
2023-11-16 07:23:07 +0200

    test ev vars

--- services/client/src/App.tsx
+++ services/client/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+const env = import.meta.env;
 
 function App() {
 	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		console.log(env);
+	});
 
 	return (
 		<>
