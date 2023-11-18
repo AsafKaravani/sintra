4765ee4344aea5b82a62ca3f6351f96c783bde08
Assaf Koren <assafnoahkoren@gmail.com>
2023-11-18 18:30:14 +0200

    fix eslint error

--- services/client/src/core/routing/ProtectedRoutes.tsx
+++ services/client/src/core/routing/ProtectedRoutes.tsx
@@ -3,8 +3,6 @@ import { Outlet, redirect } from 'react-router-dom';
 import { HomePage } from '../../view/pages/HomePage';
 import { Shell } from '../../view/layout/Shell';
 
-const sleep = (delay: number) => new Promise(resolve => setTimeout(resolve, delay));
-
 const authGuard = async () => {
 	const isAuthenticated = await checkAuth();
 	if (!isAuthenticated) {
@@ -15,7 +13,6 @@ const authGuard = async () => {
 };
 
 const checkAuth = async () => {
-	//await sleep(5000);
 	return true;
 };
 
