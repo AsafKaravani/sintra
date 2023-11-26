6c6ad6d49fee8c9547ae53252b8a2a927974d433
Assaf Koren <assafnoahkoren@gmail.com>
2023-11-27 01:29:15 +0200

    added fix for dynamic tailwind css

--- services/client/tailwind.config.js
+++ services/client/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindSafelistGenerator from 'tailwind-safelist-generator'
+
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		'./safelist.txt'
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [tailwindSafelistGenerator({
+		path: 'safelist.txt',
+		patterns: ['bg-{colors}'],
+	})],
 	important: true,
 }
 
