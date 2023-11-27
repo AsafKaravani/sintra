0929bfb47c36d01902e0ffe65ddb3293dfda41eb
Assaf Koren <assafnoahkoren@gmail.com>
2023-11-27 20:09:38 +0200

    need to fix prisma generate

--- services/client/src/core/state/features/profile/profile-state.ts
+++ services/client/src/core/state/features/profile/profile-state.ts
@@ -1,8 +1,6 @@
 import { atom } from 'recoil';
-import { Profile } from '@prisma/client';
 
 type ProfileState = {
-	profile?: Profile;
 	shouldDoOnboarding?: boolean;
 };
 export const atom_profileState = atom<ProfileState>({
